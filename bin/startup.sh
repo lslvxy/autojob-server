96a76e7c6b102fe0e244cd587dd415bd56e51115
lishan <lslvxy@163.com>
2022-05-24 11:15:33 +0800

    log

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,7 @@ APPLICATION="autojob"
 APPLICATION_JAR="${APPLICATION}.jar"
 
 cd `dirname $0`
-cd ./
+cd ./target
 BASE_PATH=`pwd`
 
 pids=$(ps -aux | grep "${APPLICATION_JAR}" | grep -v grep)
@@ -11,4 +11,4 @@ for pid in $pids;do
     kill -9 $pid
 done
 
-nohup java -Xms512m -Xmx512m -jar ${BASE_PATH}/${APPLICATION_JAR} >/dev/null &
+nohup java -jar ${APPLICATION_JAR} >/dev/null &
