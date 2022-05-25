1c30aa3636a3d9e7bba46640298e0eae8b42936c
lishan <lslvxy@163.com>
2022-05-25 20:08:18 +0800

    提供生成秘钥的方法

--- src/main/java/com/laysan/autojob/AutojobApplication.java
+++ src/main/java/com/laysan/autojob/AutojobApplication.java
@@ -1,5 +1,7 @@
 package com.laysan.autojob;
 
+import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.SecureUtil;
 import com.laysan.autojob.core.entity.Account;
 import com.laysan.autojob.core.service.AccountService;
 import com.laysan.autojob.core.utils.QuartzUtils;
@@ -13,6 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.Resource;
+import java.io.FileOutputStream;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,7 +29,28 @@ import java.util.List;
 public class AutojobApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutojobApplication.class, args);
+        if (args[0].equals("generateKey")) {
+            try {
+                KeyPair secretKey = SecureUtil.generateKeyPair("RSA");
+                PublicKey aPublic = secretKey.getPublic();
+                PrivateKey aPrivate = secretKey.getPrivate();
+                byte[] aPublicEncoded = aPublic.getEncoded();
+                String publicKeyStr = Base64.encodeStr(aPublicEncoded, false, false);
+                try (FileOutputStream fos = new FileOutputStream("./publicKey.txt")) {
+                    fos.write(publicKeyStr.getBytes());
+                    fos.flush();
+                }
+                byte[] aPrivateEncoded = aPrivate.getEncoded();
+                String privateKeyStr = Base64.encodeStr(aPrivateEncoded, false, false);
+                try (FileOutputStream fos2 = new FileOutputStream("./privateKey.txt")) {
+                    fos2.write(privateKeyStr.getBytes());
+                    fos2.flush();
+                }
+            } catch (Exception ignored) {
+            }
+        } else {
+            SpringApplication.run(AutojobApplication.class, args);
+        }
     }
 
     @Resource
