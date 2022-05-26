65c5a171255391faac73c026625661eb079b330e
lishan <lslvxy@163.com>
2022-05-26 19:28:34 +0800

    优化cloud189 日志

--- src/main/java/com/laysan/autojob/modules/cloud189/Cloud189RunService.java
+++ src/main/java/com/laysan/autojob/modules/cloud189/Cloud189RunService.java
@@ -250,8 +250,8 @@ public class Cloud189RunService implements AutoRun {
         if (result.isError()) {
             throw new BizException(result.getErrorMsg());
         }
-        AutojobContextHolder.get().appendMessage("签到获得" + result.getNetdiskBonus() + "M空间");
-        LogUtils.info(log, AccountType.MODULE_CLOUD189, account.getAccount(), "签到获得{}M空间", result.getNetdiskBonus());
+        AutojobContextHolder.get().appendMessage("签到" + result.getNetdiskBonus() + "M");
+        LogUtils.info(log, AccountType.MODULE_CLOUD189, account.getAccount(), "签到{}M", result.getNetdiskBonus());
         IoUtil.close(response);
         return result;
 
@@ -276,13 +276,7 @@ public class Cloud189RunService implements AutoRun {
             throw new BizException("抽奖失败");
         }
         if (StrUtil.isNotBlank(jsonObject.getPrizeName())) {
-            String detailMessage = AutojobContextHolder.get().getDetailMessage();
-            if (detailMessage.contains(jsonObject.getPrizeName())) {
-                AutojobContextHolder.get().appendMessage(jsonObject.getPrizeName());
-            } else {
-                AutojobContextHolder.get().appendMessage("抽奖获得" + jsonObject.getPrizeName());
-            }
-
+            AutojobContextHolder.get().appendMessage(jsonObject.getPrizeName());
             LogUtils.info(log, AccountType.MODULE_CLOUD189, AutojobContextHolder.get().getAccount(), jsonObject.getPrizeName());
         }
         IoUtil.close(response);
