eac4e3de53389e57dcc9b1f04eb8cfca3a65ea05
lishan <lslvxy@163.com>
2022-05-27 11:06:24 +0800

    fix fastjson

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,6 @@ APPLICATION="autojob"
 APPLICATION_JAR="${APPLICATION}.jar"
 
 cd `dirname $0`
-cd ./target
 BASE_PATH=`pwd`
 
 pids=$(ps -aux | grep "${APPLICATION_JAR}" | grep -v grep)
@@ -11,4 +10,4 @@ for pid in $pids;do
     kill -9 $pid
 done
 
-nohup java -jar ${APPLICATION_JAR} >/dev/null &
+nohup java -jar ../target/${APPLICATION_JAR} >/dev/null &
